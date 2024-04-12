bdf0bf0d47dcd781752006baea46df974977fe08
Vyacheslav <guzxaroh@gmail.com>
2024-04-12 18:20:23 +0700

    Добавил перечисление для меню

---
+++ HW1/HW1/MenuActions.swift
@@ -0,0 +1,15 @@
+//
+//  MenuActions.swift
+//  HW1
+//
+//  Created by Vyacheslav on 12.04.2024.
+//
+
+import Foundation
+
+enum MenuActions: String, CaseIterable {
+    case addNewCar = "1.Добавить новый автомобиль"
+    case outputCars = "2.Вывод добавленных автомобилей"
+    case outputFiltredCars = "3.Найти автомобили по кузову"
+    case exit = "4.Выйти"
+}
