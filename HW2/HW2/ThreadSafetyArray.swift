7928a76d52dd69fcefe7478c46a0c737157ecc2d
Vyacheslav <guzxaroh@gmail.com>
2024-04-19 17:25:02 +0700

    Добавил реализацию функционала

---
+++ HW2/HW2/ThreadSafetyArray.swift
@@ -0,0 +1,70 @@
+//
+//  ThreadSafetyArray.swift
+//  HW2
+//
+//  Created by Vyacheslav on 19.04.2024.
+//
+
+import Foundation
+
+class ThreadSafetyArray<T> {
+    
+    private var array: [T]
+    private let concurrentQueue = DispatchQueue(label: "ConcurrentQueue", attributes: .concurrent)
+    private let semaphore = DispatchSemaphore(value: 1)
+    
+    init (array: [T]) {
+        self.array = array
+    }
+    
+    public var count: Int {
+        self.array.count
+    }
+    
+    public var isEmpty: Bool {
+        self.array.isEmpty
+    }
+    
+    public func append(_ item: T) {
+        semaphoreActivity {
+            self.array.append(item)
+        }
+    }
+    
+    public func remove(at index: Int) {
+        semaphoreActivity {
+            if !array.isEmpty && index >= 0 && index < array.count {
+                self.array.remove(at: index)
+            }
+        }
+    }
+    
+    public func `subscript`(index: Int) -> T? {
+        var result: T?
+        
+        semaphoreActivity {
+            guard index >= 0 && index < array.count else {
+                return
+            }
+            result = array[index]
+        }
+        return result
+    }
+    
+    public func contains(_ element: T) -> Bool where T: Equatable {
+        var isContains = false
+        semaphoreActivity {
+            isContains = self.array.contains { $0 == element }
+        }
+        return isContains
+    }
+}
+
+extension ThreadSafetyArray {
+    
+    private func semaphoreActivity(completion: () -> Void) {
+        semaphore.wait()
+        completion()
+        semaphore.signal()
+    }
+}
--- HW2/HW2/main.swift
+++ HW2/HW2/main.swift
@@ -7,5 +7,32 @@
 
 import Foundation
 
-print("Hello, World!")
+var array = ThreadSafetyArray<Int>(array: [])
+let concurrentQueue = DispatchQueue(label: "ConcurrentQueue", attributes: .concurrent)
+let group = DispatchGroup()
+
+private func appendElementInArray() {
+    for number in 0...100 {
+        array.append(number)
+    }
+}
+
+private func main() {
+    group.enter()
+    concurrentQueue.async {
+        appendElementInArray()
+        group.leave()
+    }
+    
+    group.enter()
+    concurrentQueue.async {
+        appendElementInArray()
+        group.leave()
+    }
+}
+
+main()
+
+group.wait()
+print("Number of elements in array: \(array.count)")
 
