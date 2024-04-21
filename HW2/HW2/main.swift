1c7cc80a78db2df80bcb13a45d67fba62add7979
Vyacheslav <guzxaroh@gmail.com>
2024-04-21 17:30:12 +0700

    Добавил вывод элементов массива + проверка на удаление/поиск элемента

--- HW2/HW2/main.swift
+++ HW2/HW2/main.swift
@@ -12,7 +12,7 @@ let concurrentQueue = DispatchQueue(label: "ConcurrentQueue", attributes: .concu
 let group = DispatchGroup()
 
 private func appendElementInArray() {
-    for number in 0...100 {
+    for number in 0...10 {
         array.append(number)
     }
 }
@@ -34,5 +34,12 @@ private func main() {
 main()
 
 group.wait()
-print("Number of elements in array: \(array.count)")
-
+print("Number of elements in array before removing: \(array.count)")
+print(array.getData())
+array.remove(at: -1)
+array.remove(at: 0)
+array.remove(at: -1)
+print("Number of elements in array after removing: \(array.count)")
+print(array.getData())
+print(array.subscript(index: 104))
+print(array.subscript(index: 2))
