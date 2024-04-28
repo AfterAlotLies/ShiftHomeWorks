370ca6b9887b62351de0f051b5a51b4696229b59
Vyacheslav <guzxaroh@gmail.com>
2024-04-28 18:40:41 +0700

    Добавил хранение константных значений

---
+++ HW3/HW3/Resources/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  HW3
+//
+//  Created by Vyacheslav on 28.04.2024.
+//
+
+import Foundation
+import UIKit
+
+enum Colors {
+    static let blackColor: UIColor = UIColor.black
+    static let clearColor: UIColor = UIColor.clear
+    static let systemBlueColor: UIColor = UIColor.systemBlue
+    static let whiteColor: UIColor = UIColor.white
+}
