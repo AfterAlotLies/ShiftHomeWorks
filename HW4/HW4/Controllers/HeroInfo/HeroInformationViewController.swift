9637606f8f25de7710af89a3aac9b173f9507179
Vyacheslav <guzxaroh@gmail.com>
2024-05-08 20:03:49 +0700

    Добавил контроллер с подробной информацией о героях

---
+++ HW4/HW4/Controllers/HeroInfo/HeroInformationViewController.swift
@@ -0,0 +1,103 @@
+//
+//  HeroInformationViewController.swift
+//  HW4
+//
+//  Created by Vyacheslav on 06.05.2024.
+//
+
+import UIKit
+
+class HeroInformationViewController: UIViewController {
+    
+    private enum Constants {
+        static let firstYcolor = UIColor(red: 84.0 / 255.0, green: 13.0 / 255.0, blue: 13.0 / 255.0, alpha: 1)
+        static let secondYColor = UIColor(red: 26.0 / 255.0, green: 19.0 / 255.0, blue: 19.0 / 255.0, alpha: 1)
+    }
+    
+    private var heroComplexity: String = ""
+    
+    private lazy var heroInfoView: HeroInformationView = {
+        let view = HeroInformationView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        heroInfoView.setAction {
+            self.presentModalViewController()
+        }
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        coordinator.animate(alongsideTransition: nil) { context in
+            if let presentedViewController = self.presentedViewController {
+                presentedViewController.dismiss(animated: false) {
+                    self.presentModalViewController()
+                }
+            }
+            if UIDevice.current.orientation.isLandscape {
+                self.heroInfoView.updateConstraintsForOrientationChange()
+            } else {
+                self.heroInfoView.updateConstraintsForOrientationChange()
+            }
+        }
+    }
+
+    private func presentModalViewController() {
+        let heroComplexityController = HeroComplexityViewController()
+        heroComplexityController.setComplexityData(passsingComplexity: self.heroComplexity)
+        self.present(heroComplexityController, animated: true)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        if UIDevice.current.orientation.isPortrait {
+            setupBackgroundViewColor(orientation: .portrait)
+        } else {
+            setupBackgroundViewColor(orientation: .landscape)
+            heroInfoView.updateConstraintsForOrientationChange()
+        }
+    }
+    
+    func passData(data: HeroesListModel) {
+        heroInfoView.setDataToUI(data: data)
+        heroComplexity = data.complexity.rawValue
+    }
+    
+    private func setupView() {
+        view.addSubview(heroInfoView)
+        setupBackgroundViewColor(orientation: .portrait)
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            heroInfoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            heroInfoView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            heroInfoView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            heroInfoView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    private func setupBackgroundViewColor(orientation : AppOrientation) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        
+        gradientLayer.colors = [Constants.firstYcolor.cgColor, Constants.secondYColor.cgColor]
+        
+        switch orientation {
+        case .portrait:
+            gradientLayer.startPoint = CGPoint(x: 1, y: 1)
+            gradientLayer.endPoint = CGPoint(x: 0, y: 0)
+        case .landscape:
+            gradientLayer.startPoint = CGPoint(x: 1, y: 0)
+            gradientLayer.endPoint = CGPoint(x: 0, y: 0)
+        }
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+}
