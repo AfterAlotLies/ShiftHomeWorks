e4e853330f88c6aa1ad8ec187fd2aaca2fc32de0
Vyacheslav <guzxaroh@gmail.com>
2024-05-24 19:02:59 +0700

    Добавил константные значения

--- HW5/HW5/Resources/Enums/Constants.swift
+++ HW5/HW5/Resources/Enums/Constants.swift
@@ -5,4 +5,12 @@
 //  Created by Vyacheslav on 24.05.2024.
 //
 
-import Foundation
+import UIKit
+
+enum Constants {
+    static let firstYcolor = UIColor(red: 84.0 / 255.0, green: 13.0 / 255.0, blue: 13.0 / 255.0, alpha: 1)
+    static let secondYColor = UIColor(red: 26.0 / 255.0, green: 19.0 / 255.0, blue: 19.0 / 255.0, alpha: 1)
+    static let strengthImage: UIImage? = UIImage(named: "strengthImage")
+    static let agilityImage: UIImage? = UIImage(named: "agilityImage")
+    static let intelligenceImage: UIImage? = UIImage(named: "intelligenceImage")
+}
