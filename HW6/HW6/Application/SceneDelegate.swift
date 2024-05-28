bbd86d94d10a453bdb3bbc44a0f6136919a0b9d6
Vyacheslav <guzxaroh@gmail.com>
2024-05-28 16:59:33 +0700

    Добавил проект

---
+++ HW6/HW6/Application/SceneDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  SceneDelegate.swift
+//  HW6
+//
+//  Created by Vyacheslav on 24.05.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        let navigationController = UINavigationController()
+        
+        let networkManager = NetworkManager()
+        let dataSource = CarListViewDataSource()
+        let carListPresenter = CarListPresenter(networkManager: networkManager)
+        let carListViewController = CarListViewController(carListPresenter: carListPresenter, dataSource: dataSource)
+        
+        navigationController.viewControllers = [carListViewController]
+        window.rootViewController = navigationController
+        
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+
