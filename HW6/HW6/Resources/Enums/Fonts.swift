1e8c05c1707c1a6ec1949a63ec020a9b0b220d85
Vyacheslav <guzxaroh@gmail.com>
2024-05-28 17:00:37 +0700

    Добавил вспомогательные перечисления

---
+++ HW6/HW6/Resources/Enums/Fonts.swift
@@ -0,0 +1,13 @@
+//
+//  FontsEnum.swift
+//  HW6
+//
+//  Created by Vyacheslav on 28.05.2024.
+//
+
+import UIKit
+
+enum Fonts {
+    static let carListTitleScreenLabelFont: UIFont = UIFont.boldSystemFont(ofSize: 27)
+    static let carInfoPriceTextFont: UIFont = UIFont.boldSystemFont(ofSize: 23)
+}
